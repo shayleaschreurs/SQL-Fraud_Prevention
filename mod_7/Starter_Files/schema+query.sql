9ad179c344f0e99d481caef844e9f7dae9c3e484
shayleaschreurs <108375231+shayleaschreurs@users.noreply.github.com>
2022-08-25 10:46:31 -0500

    Add files via upload
    last commit - all finished

---
+++ mod_7/Starter_Files/schema+query.sql
@@ -0,0 +1,45 @@
+--schema
+--drop tables
+DROP TABLE IF EXISTS card_holder;
+DROP TABLE IF EXISTS credit_card;
+DROP TABLE IF EXISTS merchant_cat;
+DROP TABLE IF EXISTS merchant;
+DROP TABLE IF EXISTS transaction;
+
+--create tables for each csv file
+CREATE TABLE card_holder (
+    cardholder_id INT PRIMARY KEY NOT NULL
+    , customer_name VARCHAR(50)
+);
+
+CREATE TABLE credit_card (
+    card_number VARCHAR(20) PRIMARY KEY NOT NULL
+    , cardholder_id INT FOREIGN KEY REFERENCES card_holder(cardholder_id)
+);
+
+CREATE TABLE merchant_cat (
+    merchant_type_id INT PRIMARY KEY NOT NULL
+    , merchant_type VARCHAR(20) PRIMARY KEY NOT NULL
+);
+
+CREATE TABLE merchant (
+    merchant_id INT PRIMARY KEY NOT NULL
+    , merchant_name VARCHAR(100)
+    , merchant_type_id INT FOREIGN KEY REFERENCES merchant_cat(merchant_type_id)
+);
+
+CREATE TABLE transaction (
+    transaction_id INT PRIMARY KEY NOT NULL
+    , date TIMESTAMP
+    , amount VARCHAR(30)
+    , card_number VARCHAR(20) FOREIGN KEY REFERENCES credit_card(card_number)
+    , merchant_id INT FOREIGN KEY REFERENCES merchant(merchant_id)
+);
+
+--query tables
+SELECT * FROM card_holder;
+SELECT * FROM credit_card;
+SELECT * FROM merchant_cat;
+SELECT * FROM merchant;
+SELECT * FROM transaction;
+
